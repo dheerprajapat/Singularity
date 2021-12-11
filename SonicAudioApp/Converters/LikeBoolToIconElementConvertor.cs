dec64ce42377b9670ca08984ea4a6407c7c02f04
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-12-11 12:30:01 +0530

    added tooltip media duration

---
+++ SonicAudioApp/Converters/LikeBoolToIconElementConvertor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace SonicAudioApp.Converters
+{
+    public class LikeBoolToIconElementConvertor:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return null;
+            if ((bool)value)
+                return new SymbolIcon(Symbol.Dislike);
+            return new SymbolIcon(Symbol.Dislike);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
