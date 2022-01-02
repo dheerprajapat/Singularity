4c202b5bb24eed7ce43d0ef0c266240034bf26ea
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2022-01-02 18:44:21 +0530

    Revert "fixed liked song serialization"
    This reverts commit 3138327032e722b2ca39fa2c9d8249029b18606a.

--- SingularityApp/Services/LikedSongManager.cs
+++ SingularityApp/Services/LikedSongManager.cs
@@ -1,5 +1,4 @@
 ﻿using SonicAudioApp.Models;
-using SonicAudioApp.Services.YoutubeSearch;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -16,34 +15,24 @@ namespace SonicAudioApp.Services
     {
         public static ObservableCollection<AudioQueueItem> LikedSongs { get; set; } =new ObservableCollection<AudioQueueItem>();
         public static readonly string LikeInfoKeyPath = "liked.json";
-        private static List<string> SerializableSongs()=>LikedSongs.Select(s => s.Id).ToList();
+
 
         private async static void LikedSongs_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
-             var content=JsonSerializer.Serialize(SerializableSongs());
+             var content=JsonSerializer.Serialize(LikedSongs.ToList());
              await FileManager.WriteAllText(LikeInfoKeyPath, content);
         }
 
         public static async Task LoadLikedSettingsIfNotExists()
         {
             var content = await FileManager.ReadAllText(LikeInfoKeyPath);
-            if (!string.IsNullOrWhiteSpace(content))
-            {
-                var serialized = JsonSerializer.Deserialize<List<string>>(content);
-                await DesrializeSong(serialized);
-            }
+            if(!string.IsNullOrWhiteSpace(content))
+                LikedSongs=new(JsonSerializer.Deserialize<List<AudioQueueItem>>(content));
+
             foreach(var v in LikedSongs)
                 v.WaveformVisibilty = Windows.UI.Xaml.Visibility.Collapsed;
             LikedSongs.CollectionChanged += LikedSongs_CollectionChanged;
 
-        }
-        public static async Task DesrializeSong(List<string> serializedSongs)
-        {
-
-            foreach(var s in serializedSongs)
-            {
-                LikedSongs.Add(await YoutubeManager.GetInfo(s));
-            }
         }
 
         public static void Add(AudioQueueItem item)
--- SingularityApp/Services/YoutubeSearch/YoutubeManager.cs
+++ SingularityApp/Services/YoutubeSearch/YoutubeManager.cs
@@ -1,5 +1,4 @@
-﻿using SonicAudioApp.Components;
-using SonicAudioApp.Models;
+﻿using SonicAudioApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +11,7 @@ namespace SonicAudioApp.Services.YoutubeSearch
 {
     public static class YoutubeManager
     {
-        public static YoutubeClient Youtube => new YoutubeClient();
+        public static YoutubeClient Youtube = new YoutubeClient();
         public static async Task UpdateUrlAsync(AudioQueueItem c)
         {
             c.LastUpdateTimeStamp = DateTime.Now.Ticks;
@@ -21,21 +20,5 @@ namespace SonicAudioApp.Services.YoutubeSearch
             var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
             c.Url = streamInfo.Url;
         }
-        public static async Task<AudioQueueItem> GetInfo(string id)
-        {
-            var vid=await Youtube.Videos.GetAsync(id);
-            return new AudioQueueItem
-            {
-                Url = vid.Url,
-                Liked = LikedSongManager.LikedSongs.Count(x => x.Id == id) > 0,
-                WaveformVisibilty = Windows.UI.Xaml.Visibility.Collapsed,
-                Id = id,
-                Singers = vid.Author.Title,
-                Title = vid.Title,
-                ThumbnailUrl = vid.Thumbnails.OrderByDescending(x => x.Resolution.Area).First().Url,
-                DurationString = MediaPlayerControl.ConvertTimeSpanToDuration(vid.Duration.GetValueOrDefault())
-            };
-        }
-
     }
 }
