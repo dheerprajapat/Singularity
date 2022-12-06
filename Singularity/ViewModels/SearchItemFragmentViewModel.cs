cdf1982f73d7823e4a14b3398ac3e3e7cb406a64
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2022-12-06 23:14:17 +0530

    added more spacing in search

--- Singularity/ViewModels/SearchItemFragmentViewModel.cs
+++ Singularity/ViewModels/SearchItemFragmentViewModel.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Singularity.Core.Contracts.Services;
+using Singularity.Helpers;
 using Singularity.Models;
 using YoutubeExplode.Search;
 
@@ -15,7 +16,7 @@ public partial class SearchItemFragmentViewModel : ObservableRecipient
 {
     [ObservableProperty]
     public ObservableCollection<SearchFragmentItem> items;
-    public static int MaxCount=5;
+    public static int MaxCount=3;
     public SearchItemFragmentViewModel(IYoutubeService youtube)
     {
         Youtube = youtube;
@@ -32,16 +33,16 @@ public partial class SearchItemFragmentViewModel : ObservableRecipient
 
         var items =  Youtube.GetSearchResult("die for you",SearchType.Video);
         var res = new List<SearchFragmentItem>();
-        int k = 0;
+        var k = 0;
         await foreach (VideoSearchResult i in items)
         {
             res.Add(new()
             {
-            ChannelName=i.Author.ChannelTitle,
-            MediaType="Video",
-            Name=i.Title,
-            Duration=i.Duration.ToString(),
-            ThumbnailUrl=i.Thumbnails.OrderByDescending(x=>x.Resolution.Area).FirstOrDefault().Url
+                ChannelName=i.Author.ChannelTitle,
+                MediaType="Video",
+                Name=i.Title,
+                Duration=MediaPlayerHelper.ConvertTimeSpanToDuration(i.Duration.GetValueOrDefault()),
+                ThumbnailUrl = i.Thumbnails.OrderByDescending(x => x.Resolution.Area).FirstOrDefault().Url
             });
             k++;
             if (k >= MaxCount)
