f0eb9f8786e7a88353094296e470560726833593
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2022-12-23 22:16:32 +0530

    cleanup adding json data

---
+++ Singularity.Core/Models/YoutubeFeed.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using YoutubeExplode.Playlists;
+
+namespace Singularity.Core.Models;
+
+public class Genre
+{
+    public Genre(
+        string name,
+        List<string> playlistIds
+    )
+    {
+        this.Name = name;
+        this.PlaylistIds = playlistIds;
+    }
+
+    [JsonProperty("name")]
+    [JsonPropertyName("name")]
+    public string Name
+    {
+        get;
+    }
+
+    [JsonProperty("playlistIds")]
+    [JsonPropertyName("playlistIds")]
+    public IReadOnlyList<string> PlaylistIds
+    {
+        get;
+    }
+}
+
+public class YoutubeFeed
+{
+    public YoutubeFeed(
+        List<Genre> genres,
+        List<string> trendingVideosIds
+    )
+    {
+        this.Genres = genres;
+        this.TrendingVideosIds = trendingVideosIds;
+    }
+
+    [JsonProperty("genres")]
+    [JsonPropertyName("genres")]
+    public IReadOnlyList<Genre> Genres
+    {
+        get;
+    }
+
+    [JsonProperty("trendingVideosIds")]
+    [JsonPropertyName("trendingVideosIds")]
+    public IReadOnlyList<string> TrendingVideosIds
+    {
+        get;
+    }
+}
+
--- Singularity/ViewModels/HomeViewModel.cs
+++ Singularity/ViewModels/HomeViewModel.cs
@@ -1,20 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Singularity.Core.Helpers;
-using Singularity.Core.Models.YoutubeJS;
 using Singularity.Core.Services;
 
 namespace Singularity.ViewModels;
 
 public partial class HomeViewModel : ObservableRecipient
 {
-    [ObservableProperty]
-    public MusicFeed? feed;
 
     public HomeViewModel()
-    {
-    }
-
-    private void DenoServerLauncher_DenoServerStarted()
     {
         LoadFeedAsync();
     }
