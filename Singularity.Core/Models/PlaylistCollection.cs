91a258a81811c8e8e70dc3ae1d064b971c17b897
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2023-01-08 13:20:54 +0530

    Update PlaylistCollection.cs

--- Singularity.Core/Models/PlaylistCollection.cs
+++ Singularity.Core/Models/PlaylistCollection.cs
@@ -1,34 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Singularity.Core.Contracts.Services;
+using Singularity.Core.Services;
+using YoutubeExplode.Playlists;
 
 namespace Singularity.Core.Models;
 public class PlaylistCollection
 {
-
+    public ObservableCollection<PlaylistCollection> Playlists { get; set; } = new();
 }
+
+#nullable enable
 public class PlaylistItem
 {
-    public PlaylistItemType PlaylistType = PlaylistItemType.Custom;
-    [JsonIgnore]
-    public  bool IsReadOnly => PlaylistType == PlaylistItemType.Default;
+
     public string Name
     {
-        get;set;
+        get; set;
     }
-}
+    public string Author
+    {
+        get; set;
+    }
+    public ObservableCollection<string> Songs
+    {
+        get; set;
+    } = new ObservableCollection<string>();
 
-public enum PlaylistItemType
-{
-    /// <summary>
-    /// youtubes default playlist
-    /// </summary>
-    Default,
-    /// <summary>
-    /// Includes songs from outside youtube's builtin playlist
-    /// </summary>
-    Custom 
+    public string ThumbnailUrl
+    {
+        get; set;
+    }
+    public PlaylistItem(string name, string author, ObservableCollection<string> songs, string thumbnailUrl)
+    {
+        Name = name;
+        Author = author;
+        Songs = songs;
+        ThumbnailUrl = thumbnailUrl;
+    }
 }
