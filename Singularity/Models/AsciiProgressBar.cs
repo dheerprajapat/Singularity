1e080e4dc5f25433ae7c38af0ee067eb2a9bbf86
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2023-01-18 22:22:16 +0530

    Create AsciiProgressBar.cs

---
+++ Singularity/Models/AsciiProgressBar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singularity.Models;
+internal class AsciiProgressBar
+{
+
+    static readonly char DashLight = '─';
+    static readonly char DashThick = '━';
+    static readonly char CircleCenter = '⬤';
+    public static string GetProgressAscii(int value, int max, int size = 15)
+    {
+        int thickDashCount = (int)(value * size * 1.0f / max);
+        var result = "".PadLeft(thickDashCount, DashThick);
+        result += CircleCenter;
+        result += "".PadLeft(size - thickDashCount, DashLight);
+        return result;
+    }
+}
+
