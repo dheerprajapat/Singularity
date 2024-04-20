dfb4e7464aad91d903e51b855700bedd8e16af74
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2024-04-20 16:00:57 +0530

    minimize music view on back button

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,11 +6,19 @@ namespace Singularity
     {
 #nullable disable
         public static MediaElement MediaElement { get; private set; }
+        public static Func<bool> OnBackButtonPress { get; set; }
 #nullable restore
         public MainPage()
         {
             InitializeComponent();
             MediaElement = MediaUIElement;
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            if(OnBackButtonPressed == null)
+                return base.OnBackButtonPressed();
+            return OnBackButtonPress.Invoke();
         }
     }
 }
